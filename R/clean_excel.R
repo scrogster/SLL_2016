f23dcd49b8af60c8b4d497ee30ab3d83d36830bf
scrogster <scrogster@gmail.com>
2016-08-16 01:22:57 +1000

    Good solid beginning on filtering the survey data. Just need to impute missing vals and check everything.

---
+++ R/clean_excel.R
@@ -0,0 +1,72 @@
+require(readxl)
+require(dplyr)
+require(tidyr)
+
+wcma<-read_excel("DataFromGarry/SLL monitoring database formatted 13May13.xls", 1)
+ccma<-read_excel("DataFromGarry/SLL monitoring database formatted 13May13.xls", 2)
+ghcma<-read_excel("DataFromGarry/SLL monitoring database formatted 13May13.xls", 3)
+
+#filter out blank lines
+wcma <- wcma %>% filter(!is.na(`Grid No`)) %>% select (-`Time End (EST)`) %>% mutate(CMA="wcma")
+ccma <- ccma %>% filter(!is.na(`Grid No`)) %>% select (-`Time End (EST)`) %>% mutate(CMA="ccma")
+ghcma <- ghcma %>% filter(!is.na(`Grid No`)) %>% select (-`Time End (EST)`) %>% mutate(CMA = "ghcma")
+
+combined_raw<-rbind(wcma, ccma, ghcma)
+
+
+#first cut of a filtering pipe for the data.
+
+#counting of Delma adults and skins not quite right yet (2nd last line)
+
+DelmaFiltered<- combined_raw %>%
+	select(Grid=`Grid No`, Cluster, CMA, Date, Season=`Field Season`,
+				 T1=`Time Start (EST)`,
+				 TempA1=`Temp (a) Start`, TempA2=`Temp (a) End`, TempS1=`Temp (s) Start`, TempS2=`Temp (s) End`,
+				 HumidA1=`Humidity (a) Start`, HumidA2=`Humidity (a) End`, HumidS1=`Humidity (s) Start`,  HumidS2=`Humidity (s) End`,
+				 Sun1=`Sun Start`,  Sun2=`Sun End`,
+				 Cloud1=`Cloud Cover Start`, Cloud2=`Cloud Cover End`,
+				 `Species`, `Number`, `Evidence`) %>%
+	 select(Grid, Cluster, CMA, Season, Date, Time=T1, AirTemp = TempA1, SoilTemp=TempS1, 
+	 			 HumidA=HumidA1, HumidS=HumidS1, Sun=Sun2, Cloud=Cloud1, Species, Number, Evidence) %>%
+	   mutate(GridCMA = paste0(Grid, CMA), Number=as.numeric(gsub("[^1-9]", "", Number))  ) %>%
+	group_by(CMA, GridCMA, Date) %>%
+	summarise(Time=first(Time), Grid=first(Grid), Cluster=first(Cluster), Season=first(Season),
+						AirTemp=first(AirTemp), SoilTemp=first(SoilTemp), HumidA=first(HumidA), HumidS=first(HumidS), 
+						Sun=first(Sun), Cloud=first(Cloud), 
+						DelmaLizards = sum(Number[Species=="Delma impar" & is.na(Evidence)], na.rm=TRUE),
+						DelmaOther = sum(Number[Species=="Delma impar" & !is.na(Evidence)] , na.rm=TRUE))%>%
+	arrange(GridCMA, CMA, Cluster, Grid, Date)
+
+#we'll need to make a single date/time object variable
+
+#there's some missing dates and times (impute by year average?)
+DelmaFiltered[which(is.na(DelmaFiltered$Time)|is.na(DelmaFiltered$Date)),]
+
+hist(hour(DelmaFiltered$Time))
+hist(minute(DelmaFiltered$Time))
+table(year(DelmaFiltered$Date))
+table(month(DelmaFiltered$Date)) #a few am/pm confusions, but fixable - assume <6 is actually pm.
+table(day(DelmaFiltered$Date))
+
+X<-ymd_h(paste(
+	year(DelmaFiltered$Date),
+	month(DelmaFiltered$Date),
+	day(DelmaFiltered$Date),
+	hour(DelmaFiltered$Time)))
+	
+))
+
+#ghcma 123
+#ccma 94
+#wcma 78
+
+78+94+123
+
+#seem to be dropping sites for some reason....
+length(unique(DelmaFiltered$Grid))
+			 length(unique(wcma$`Grid No`))+
+			 length(unique(ccma$`Grid No`))+
+			 length(unique(ghcma$`Grid No`))
+save.image
+
+
