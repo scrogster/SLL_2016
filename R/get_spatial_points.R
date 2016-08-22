786351e0413d26cddb4ce941f1927d1cb9fe417c
scrogster <scrogster@gmail.com>
2016-08-22 22:41:54 +1000

    Doing extraction from grassland raster

--- R/get_spatial_points.R
+++ R/get_spatial_points.R
@@ -7,10 +7,10 @@ library(rgdal)
 library(stringr)
 library(tidyr)
 
+load("prepped_data.Rdata")
 #read in Garry's point data from excel
 #contrary to what the spreadsheets say, all data is in the first (GHCMA) leaf.
 site_points<-read_excel("SpatialData/DelmaGridVariables_22Aug2016.xls",1)
-
 
 site_points<-site_points %>%
 	select(Site, LandUse=`Land use`, LandUseCode=`Aggr. Land Use`, 
@@ -29,7 +29,7 @@ sites_points_55<-SpatialPoints(coords=site_points[s55,c("Easting", "Northing")],
 sites_points_54<-SpatialPoints(coords=site_points[s54,c("Easting", "Northing")], 
 							proj4string = CRS("+init=epsg:28354")) #28354
 
-#Conver to vicgrid. Because of the split, these will be out of order. Fix once we've got what we need.
+#Convert to vicgrid. Because of the split, these will be out of order. Fix once we've got what we need.
 sites_points_55_VG<-spTransform(sites_points_55, CRS("+init=epsg:3111"))
 sites_points_54_VG<-spTransform(sites_points_54, CRS("+init=epsg:3111"))
  plot(sites_points_54_VG, pch=16)
@@ -44,10 +44,6 @@ vicgrid_spdf<-SpatialPointsDataFrame(vicgrid_points, data.frame(site_points[!is.
 
 plot(vicgrid_points, pch=16) 
 
-writeOGR(vicgrid_spdf, "SpatialData", "GridPoints", driver = "ESRI Shapefile", overwrite_layer = TRUE)
-
-load("prepped_data.Rdata")
-
 #experimental merge between site point data and survey data
 test<-left_join(ungroup(DelmaFiltered), site_points, "GridCMA") %>%
 	select(GridCMA, Easting, Northing)
@@ -58,19 +54,30 @@ unique(test$GridCMA[which(is.na(test$Northing))])
 
 #a bit more info about these sites....
 
+#Garry not too sure about these sites - only single surveys, so exclude.
 #"10.3.2ccma"   10.3.2 Bannockburn #single survey only in 2010 EXCLUDE
 #"10.3.3ccma"   10.3.3 Bannockburn  #single survey only in 2010 EXCLUDE
 DelmaFiltered %>% filter(GridCMA=="10.3.2ccma") %>% select(Date, DelmaLizards, DelmaOther)
 DelmaFiltered %>% filter(GridCMA=="10.3.3ccma") %>% select(Date, DelmaLizards, DelmaOther)
 
+#Ted has information about these sites, but no coordinates. Only a single survey at each
 #"17.3.1ghcma"  17.3.1     col    #Single survey only in 2005 EXCLUDE
 #"17.6.1ghcma"  17.6.1     col    #Single survey only in 2005 EXCLUDE
 DelmaFiltered %>% filter(GridCMA=="17.3.1ghcma") %>% select(Date, DelmaLizards, DelmaOther)
 DelmaFiltered %>% filter(GridCMA=="17.6.1ghcma") %>% select(Date, DelmaLizards, DelmaOther)
 
-#"10.2.2wcma"  10.2.2 Longerenong #Surveyed in 2010, 2011, 2012.
-#"19.3.1wcma"   19.3.1 Lallat Plain #Surveyed in 2010, 2011, 2012.
-#"20.8.1wcma"   20.8.1  Minyip  #Surveyed in 2010, 2011
-DelmaFiltered %>% filter(GridCMA=="10.2.2wcma") %>% select(Date, DelmaLizards, DelmaOther)
-DelmaFiltered %>% filter(GridCMA=="19.3.1wcma") %>% select(Date, DelmaLizards, DelmaOther)
-DelmaFiltered %>% filter(GridCMA=="20.8.1wcma") %>% select(Date, DelmaLizards, DelmaOther)
+
+library(raster)
+
+grassland<-raster("Rasters/NatGrassland_clip.tif")
+
+#in the raster 2 =very likely grassland, 1=likely grassland
+grass1<-extract(grassland, vicgrid_spdf, buffer=500, fun=function(x,...){sum(x==1)/length(x)}, na.rm=TRUE)
+grass2<-extract(grassland, vicgrid_spdf, buffer=500, fun=function(x,...){sum(x==2)/length(x)}, na.rm=TRUE)
+
+vicgrid_spdf$grass1<-grass1
+vicgrid_spdf$grass2<-grass2
+
+writeOGR(vicgrid_spdf, "SpatialData", "GridPoints", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+
+save.image("prepped_data_plusGIS.Rdata")
