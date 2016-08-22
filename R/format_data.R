64ceff7d791be179cea24ac603cb26e350463179
michael scroggie <scrogster@gmail.com>
2016-08-23 09:27:40 +1000

    updated code

--- R/format_data.R
+++ R/format_data.R
@@ -1,7 +1,8 @@
 
 require(jagsUI)
 require(lubridate)
-load("prepped_data.Rdata")
+require(dplyr)
+load("prepped_data_plusGIS.Rdata")
 
 #variables to get for model
 
@@ -18,6 +19,28 @@ SurvHour[is.na(SurvHour)]<-mean(SurvHour, na.rm=TRUE)
 SurvMinute[is.na(SurvMinute)]<-30
 timeofday<-(SurvHour + SurvMinute/60)-12
 
+#getting the grassland measures matched up with the survey data. 
+xx<-DelmaFiltered %>%
+	ungroup() %>%
+	select(GridCMA, Date) %>%
+	left_join(data.frame(vicgrid_spdf), by="GridCMA") %>%
+	select(GridCMA, Date, grass1, grass2, LandUse, FireHistory) %>%
+	mutate(grasstot = grass1+grass2) %>%
+	group_by(GridCMA) %>%
+	summarise(grass1=first(grass1), grass2=first(grass2), grasstot=first(grasstot), 
+						LandUse=first(LandUse), FireHistory=first(FireHistory)) %>%
+	mutate(Conservation=grepl("Conservation", LandUse), Grazing=grepl("Grazing", LandUse))
+#check that sites are in same order as sites in detection data:
+all.equal(levels(factor(DelmaFiltered$GridCMA)) , xx$GridCMA)
+
+#make the site variables:
+grassland<-xx$grasstot
+grassland[which(is.na(grassland))]<-mean(grassland, na.rm=TRUE)
+grazing<-xx$Grazing
+#let's get the site variables...
+#we need to get the matching values for grassland area, (cons/grazing/roadside), fire. Need to match up 
+#with variable "i" (site index) in occ model.
+
 jags_dat<-list(
   tot.sites= max(as.numeric(factor(DelmaFiltered$GridCMA))),
   max.time= max(DelmaFiltered$yearnum),
@@ -28,7 +51,9 @@ jags_dat<-list(
 	t = DelmaFiltered$yearnum,
   year.frac = DelmaFiltered$yeardayfrac,
   TempS = TempS,
-  time.of.day=timeofday
+  time.of.day=timeofday,
+  grassland=grassland,
+  grazing=grazing*1.0 #convert Boolean to numeric
 )
 
 modfile <- 'R/prototype_occmod.txt'
@@ -56,7 +81,7 @@ out <- jags(data = jags_dat,
 						parallel=FALSE, 
 						n.chains = 3,
 						n.adapt = 100,
-						n.iter = 1500,
+						n.iter = 1000,
 						n.burnin = 500,
 						n.thin = 2)
 
@@ -74,10 +99,11 @@ aa<-BETA[1]+BETA[2]*(cos(2*pi*x))+BETA[3]*(sin(2*pi*x)) +
 bb<-BETA[9]+BETA[10]*(cos(2*pi*x))+BETA[11]*(sin(2*pi*x))+ 
 	BETA[12]*(cos(4*pi*x))+BETA[13]*(sin(4*pi*x))
 
-
+pdf("Figures/detection_curve.pdf", width=8, height=8)
 plot(plogis(aa)~x, col="red", type="l", ylim=c(0, 1), lwd=2, ylab="Pr(detect)",
 		 xlab="Fraction of Calendar Year", las=1)
 lines(y=plogis(bb), x=x, col="blue", lwd=2)
 lines(y = 1- (1-plogis(aa))*(1-plogis(bb)), x=x, col="green", lwd=2)
 title(main=c("Red=lizards, Blue=sloughs, Green=combined"))
+dev.off()
 
