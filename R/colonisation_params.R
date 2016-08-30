f0197f31a90ba40000beca7f8bfe3f0d52c7a9e7
scrogster <scrogster@gmail.com>
2016-08-30 15:25:00 +1000

    initial commit of script to plot colonisation parameters

---
+++ R/colonisation_params.R
@@ -0,0 +1,24 @@
+require(jagsUI)
+require(ggplot2)
+require(dplyr)
+require(tidyr)
+
+load("fitted_model.Rdata")
+
+#plotting initial occupancy parameters
+df<-data.frame(out$sims.list$D) %>%
+	rename(Intercept=X1, Grassland=X2, Fire=X3, Grazing=X4) %>%
+	gather() %>%
+	mutate(key=factor(key, levels=c("Intercept", "Grassland", "Fire", "Grazing"))) %>%
+	filter(key != "Fire" & key != "Grazing") #we haven't put fire in the model yet, so don't estimate.
+
+histocol="green"
+
+ggplot(df, aes(x=value)) +
+	geom_histogram(col=histocol, fill=histocol, binwidth=0.05) +
+	geom_vline(xintercept=0) +
+	facet_grid(key~., scales="fixed") +
+	ggtitle("Probability of colonisation") +
+	theme_bw()
+
+ggsave("Figures/colonisation_params.pdf", width=5, height=3.5)
