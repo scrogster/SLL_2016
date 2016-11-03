d6c0869a3aa3e76f618c3098d9824f887973676d
michael scroggie <scrogster@gmail.com>
2016-11-03 15:19:09 +1100

    render script now respects filtype argument

--- R/render.R
+++ R/render.R
@@ -1,4 +1,10 @@
 library(rmarkdown)
 
-render("Delma.Rmd")
-render("Delma.Rmd", "pdf_document")
+args = commandArgs(trailingOnly=TRUE)
+
+#desired format for ouput paper
+out_format=args[2]
+
+if(out_format=="word_document"){render("Delma.Rmd", "word_document")}
+if(out_format=="pdf_document") {render("Delma.Rmd", "pdf_document")}
+
