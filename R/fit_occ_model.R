97ba48d4a4b770f273d57073853108ae6cf5f0fd
michael scroggie <scrogster@gmail.com>
2017-04-27 17:25:25 +1000

    starting vals for z at undetected sites are now assigned randomly.

--- R/fit_occ_model.R
+++ R/fit_occ_model.R
@@ -6,10 +6,10 @@ load("formatted_for_JAGS.Rdata")
 
 #MCMC settings
 n.chains=4
-n.adapt=1000
-n.iter=40000
+n.adapt=2000
+n.iter=60000
 n.burnin=20000
-n.thin=10
+n.thin=20
 para=TRUE
 
 
@@ -24,18 +24,30 @@ z.init<-
 	tapply(DelmaFiltered$DelmaLizards+DelmaFiltered$DelmaOther, 
 				 list(DelmaFiltered$GridCMA, DelmaFiltered$yearnum), 
 				 function(x){min(sum(x), 1)  }  ) 
+
+#function to assign initial occupancy states to the Z matrix.
+z.initiator<-function(){
+z.init<-
+	tapply(DelmaFiltered$DelmaLizards+DelmaFiltered$DelmaOther, 
+				 list(DelmaFiltered$GridCMA, DelmaFiltered$yearnum), 
+				 function(x){min(sum(x), 1)  }  ) 
 z.init[z.init==0]<-NA #just assign one to years with known occurences. otherwise NA for init of Z.
+#number of sites that are NA
+N.na<-length(z.init[is.na(z.init)])
+z.init[is.na(z.init)]<-rbinom(N.na, 1, 0.1)
+return(z.init)
+}
 
-
+#function to generate starting values
 inits <- function(){  
-	list(B=rnorm(5,0,0.5),
-			 C=rnorm(6,0,0.5),
-			 D=rnorm(2,0,0.5), 
+	list(B=rnorm(5,0,0.2),
+			 C=rnorm(6,0,0.2),
+			 D=rnorm(2,0,0.2), 
 			 BETA=rnorm(15,0,0.2),
-			 Z=z.init,
-			 clus_var_occ=runif(1, 0, 10),
-			 clus_var_persist=runif(1, 0, 10),
-			 clus_var_colon=runif(1, 0, 10))
+			 Z=z.initiator(),
+			 clus_var_occ=runif(1, 1, 10),
+			 clus_var_persist=runif(1, 1, 10),
+			 clus_var_colon=runif(1, 1, 10))
 }
 
 out <- jags(data = jags_dat,
