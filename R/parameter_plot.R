e190e7fc1805063005d607e1fd516a53be10e192
michael scroggie <scrogster@gmail.com>
2017-06-09 16:37:20 +1000

    updated plot code with new model structure from prev commit

--- R/parameter_plot.R
+++ R/parameter_plot.R
@@ -8,9 +8,9 @@ load("fitted_model.Rdata")
 
 #plotting initial occupancy parameters
 df<-data.frame(out$sims.list$B) %>%
-	rename(Intercept=X1, Grassland=X2, Clay=X3, Grazing1=X4, Grazing2=X5) %>%
+	rename(Intercept=X1, Grassland=X2, Clay=X3, Roadside=X4) %>%
 	gather() %>%
-	mutate(key=factor(key, levels=c("Intercept", "Grassland", "Clay","Grazing1", "Grazing2"))) 
+	mutate(key=factor(key, levels=c("Intercept", "Grassland", "Clay","Roadside"))) 
 
 
 INITPLOT<-ggplot(df, aes(y=value, x=key)) +
@@ -23,9 +23,9 @@ INITPLOT<-ggplot(df, aes(y=value, x=key)) +
 
 #plotting the persistence parameters
 df<-data.frame(out$sims.list$C) %>%
-	rename(Intercept=X1, Grassland=X2, Fire=X3, Firegt4=X4, Grazing=X5, Grazing2=X6) %>%
+	rename(Intercept=X1, Grassland=X2, Fire=X3, Fire2=X4, Grazing=X5, Grazing2=X6) %>%
 	gather() %>%
-	mutate(key=factor(key, levels=c("Intercept", "Grassland", "Fire", "Firegt4", "Grazing", "Grazing2"))) 
+	mutate(key=factor(key, levels=c("Intercept", "Grassland", "Fire", "Fireg2", "Grazing", "Grazing2"))) 
 
 PERSISTPLOT<-ggplot(df, aes(y=value, x=key)) +
 	geom_hline(yintercept=0, linetype=2) +
