6b063c0f4caadf82f7c8eed0237c3b7955e00ec3
michael scroggie <scrogster@gmail.com>
2017-10-13 13:41:02 +1100

    added scalebar to site map

--- R/Site-map.R
+++ R/Site-map.R
@@ -39,23 +39,40 @@ sample_points<-data.frame(grassjoin, ever.detect) %>%
 lizpoints<-SpatialPointsDataFrame(cbind(sample_points$Easting, sample_points$Northing), data=sample_points)
 proj4string(lizpoints)=CRS("+init=epsg:3111")
 
-lizpoints$colvec[lizpoints$ever.detect==1]<-'red'
-lizpoints$colvec[lizpoints$ever.detect==0]<-'blue'
-lizpoints$pchvec[lizpoints$ever.detect==1]<-16
-lizpoints$pchvec[lizpoints$ever.detect==0]<-1
+lizpoints$colvec[lizpoints$ever.detect==1]<-'black'
+lizpoints$colvec[lizpoints$ever.detect==0]<-'black'
+lizpoints$pchvec[lizpoints$ever.detect==1]<-2
+lizpoints$pchvec[lizpoints$ever.detect==0]<-16
 lizpoints$cexvec[lizpoints$ever.detect==1]<-0.5
-lizpoints$cexvec[lizpoints$ever.detect==0]<-0.4
+lizpoints$cexvec[lizpoints$ever.detect==0]<-0.3
 
 #make WGS84 versions so that map has lat/longs
 lizpoints_wgs84<-spTransform(lizpoints, CRS("+init=epsg:4326")	)
 VIC_simple_wgs84<-spTransform(VIC_simple, CRS("+init=epsg:4326")	)
 aus_map_simple_wgs84<-spTransform(aus_map_simple, CRS("+init=epsg:4326")	)
 
+
+#scale bar calculations
+bar_length=100  #length of scale bar in km
+scale_start<-SpatialPoints(matrix(c(148.4, -38.5), nrow=1), CRS("+init=epsg:4326"))
+scale_start_VG<-spTransform(scale_start,  CRS("+init=epsg:3111"))
+scale_end_VG<-SpatialPoints(matrix(coordinates(scale_start_VG)+c(bar_length*1000, 0), nrow=1),  CRS("+init=epsg:3111"))
+scale_end<-spTransform(scale_end_VG,  CRS("+init=epsg:4326"))
+
+#these are lat-long scale coords for the start and end of the scale bar:
+x0<-coordinates(scale_start)[1]
+x1<-coordinates(scale_end)[1]
+y0<-coordinates(scale_start)[2]
+y1<-coordinates(scale_end)[2]
+
+
+shade=0.85  #grey shade for map.
+
 #make a map with an inset
 pdf("Figures/Site-map.pdf", width=7, height=5)
 par(mar=c(3, 3.3, 0.25, 0.25))
 grid_col=gray(0.5)
-plot(VIC_simple_wgs84, col=gray(0.8))
+plot(VIC_simple_wgs84, col=gray(shade))
 plot(lizpoints_wgs84, col=lizpoints$colvec, add=TRUE, pch=lizpoints$pchvec, cex=lizpoints$cexvec)
 longdegs=seq(140, 150, by=2)
 a = sapply(longdegs,function(x) bquote(.(x)*degree ~ E))
@@ -63,14 +80,17 @@ axis(1, at=longdegs, lab=do.call(expression, a),las=1)
 latdegs=seq(-34, -39, by=-1)
 b = sapply(latdegs,function(x) bquote(.(-x)*degree ~ S))
 axis(2, at=latdegs, lab=do.call(expression, b),las=1)
+#scale bar
+arrows(x0=x0, y0=y0, x1=x1, y1=y1, col="black", lwd=2, code=3, angle=90, length=0.05)
+text(x=(x0+x1)/2, y=-0.25+(y0+y1)/2, labels=paste(bar_length, "km"), adj=0.5)
 box()
 legend(x=143.8, y=-33.9, legend=
 			 	c(expression(paste(italic("D. impar"), " detected" )  ), 
 			 		expression(paste(italic("D. impar"), " not detected" )  )),
-			   pch=c(16, 1), col=c('red', 'blue'), pt.cex=c(0.5, 0.4))
+			   pch=c(2, 16), col=c('black', 'black'), pt.cex=c(0.5, 0.3))
 par(fig = c(.75, 0.99, .7, 0.985), mar=c(0,0,0,0), new=TRUE)
 plot(ausmain)
-plot(VIC, add=TRUE, col=gray(0.8))
+plot(VIC, add=TRUE, col=gray(shade))
 box()
 dev.off()
 
