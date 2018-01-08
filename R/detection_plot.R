94fbc7c3743188f5fc5046ae280fab90cd5e9f33
michael scroggie <scrogster@gmail.com>
2018-01-08 16:16:54 +1100

    detection model was using tile temperature, now using air temperature. this was an error. detection plot also updated to reflect altered model structure.

--- R/detection_plot.R
+++ R/detection_plot.R
@@ -52,54 +52,55 @@ SP<-ggplot(combined, aes(y=mean, x=x, group=Type, col=Type, linetype=Type))+
 	ylim(0, 1)+
 	scale_x_continuous(breaks = cumsum(c(1, 31, 30, 28, 30, 31, 30, 31, 31, 30, 31, 30,31)), 
 										 label = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 
-										 					'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan'), expand = c(0, 0))+
+										 					'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', ''), expand = c(0, 0))+
 	annotate("text", x=Inf, y=Inf, label="A", vjust=1.2, hjust=1.1, size=10)+
 	theme_bw()+
 #	theme(axis.text.x=element_text(hjust=-1.8))+
-	theme(legend.position=c(0.16, 0.84))+
+	theme(legend.position=c(0.15, 0.9))+
 	theme(legend.title=element_blank())+
+	theme(legend.key.size = unit(0.3, "cm")) +
 	theme(legend.background=element_rect(colour="black", fill="white", size=0))+
 	theme(legend.key=element_rect(colour=NA, size=0))
 
 
 #Response to under-tile temperature ----############################################
 #Temperature response plot
-Temp<-seq(0, 50)  #Undertile temps between 0 and 50
-Temp<-t(Temp) 
-dd<-t(rep(10/12, length(Temp)))  #early November
-Tdiff=t(rep(0, length(Temp)))   #No air-soil Temperature difference
+TempA<-seq(0, 50)  #Air temps between 0 and 50
+TempA<-t(TempA) 
+dd<-t(rep(10/12, length(TempA)))  #early November
+Tdiff=t(rep(0, length(TempA)))   #No air-soil Temperature difference
 
 #curve for lizards
 Tpred<-BETA[,1]+BETA[,2]%*%(cos(2*pi*dd))+BETA[,3]%*%(sin(2*pi*dd)) + 
 	BETA[,4]%*%(cos(4*pi*dd))+BETA[,5]%*%(sin(4*pi*dd)) +
-	BETA[,8]%*%((Temp-22)/5) +
-	BETA[,9]%*%(((Temp-22)/5)^2) +
+	BETA[,8]%*%((TempA-22)/5) +
+	BETA[,9]%*%(((TempA-22)/5)^2) +
 	BETA[,10]%*%Tdiff +
-	BETA[,11]%*% (((Temp-22)/5)*Tdiff ) #interaction term.
+	BETA[,11]%*% (((TempA-22)/5)*Tdiff ) #interaction term.
 #Summary stats in a curve....
-Tcurve<-data.frame("x"=t(Temp), summary_func(plogis(Tpred)))
+Tcurve<-data.frame("x"=t(TempA), summary_func(plogis(Tpred)))
 
 Tplot<-ggplot(Tcurve, aes(y=mean, x=x))+
 	geom_ribbon(aes(ymin=lwr, ymax=upp), col=NA, fill="green", alpha=0.2)+
 	geom_ribbon(aes(ymin=lqt, ymax=uqt), col=NA, fill="green", alpha=0.4)+
 	geom_line(col="green")+
 	ylab("Probability of detection")+
-	xlab("T (tile)")+
+	xlab("T (air)")+
 	ylim(0, 1)+
 	annotate("text", x=Inf, y=Inf, label="B", vjust=1.2, hjust=1.1, size=10)+
 	theme_bw()
 
 #Response to temperature differential ----############################################
-Temp<-rep(25, 50)  #Undertile temp of 25
-Temp<-t(Temp) 
-dd<-t(rep(10/12, length(Temp)))  #early November
-Tdiff=t(seq(-20, 20, length.out=length(Temp)))   #No air-soil Temperature difference
+TempA<-rep(25, 50)  #Air temp of 25
+TempA<-t(TempA) 
+dd<-t(rep(10/12, length(TempA)))  #early November
+Tdiff=t(seq(-20, 20, length.out=length(TempA)))   #range of temp differences
 
 #curve for lizards
 Tdiffpred<-BETA[,1]+BETA[,2]%*%(cos(2*pi*dd))+BETA[,3]%*%(sin(2*pi*dd)) + 
 	BETA[,4]%*%(cos(4*pi*dd))+BETA[,5]%*%(sin(4*pi*dd)) +
-	BETA[,8]%*%((Temp-22)/5) +
-	BETA[,9]%*%(((Temp-22)/5)^2) +
+	BETA[,8]%*%((TempA-22)/5) +
+	BETA[,9]%*%(((TempA-22)/5)^2) +
 	BETA[,10]%*%Tdiff 
 
 #Summary stats in a curve....
@@ -120,8 +121,8 @@ Tdiffplot<-ggplot(Tdiffcurve, aes(y=mean, x=x))+
 
 ##raster plot of response
 
-pred_data<-expand.grid(Temp=5:60, TempA=5:50, TOD=0) %>%
-	        mutate(Tdiff=Temp-TempA)
+pred_data<-expand.grid(TempS=0:61, TempA=0:43, TOD=0) %>%
+	        mutate(Tdiff=TempS-TempA)
 
 beta_means<-colMeans(BETA)
 
@@ -133,58 +134,46 @@ pred_detect<-function(predrow){
 p.liz<-beta_means[1] +fourier.liz  + 
 	beta_means[6]*predrow["TOD"]+
 	beta_means[7]*(predrow["TOD"]^2)+  
-	beta_means[8]*((predrow["Temp"]-22)/5) +
-	beta_means[9]*((predrow["Temp"]-22)/5)^2 +
+	beta_means[8]*((predrow["TempA"]-22)/5) +
+	beta_means[9]*((predrow["TempA"]-22)/5)^2 +
 	beta_means[10]*predrow["Tdiff"] +   #effect of air-soil temp diff. 
-	beta_means[11]*((predrow["Temp"]-22)/5)*predrow["Tdiff"]
+	beta_means[11]*((predrow["TempA"]-22)/5)*predrow["Tdiff"]
 return(plogis(p.liz))
 }
 
 out<-apply(pred_data, 1, pred_detect)
 out<-cbind(pred_data, p=out)
 
-dd<-data.frame(TempA=jags_dat$TempA, TempS=jags_dat$TempS, Tdiff=TempS-TempA, 
-							 yfrac=jags_dat$year.frac, detect=factor(jags_dat$detect.liz)) %>%
-	     filter(yfrac>9/12)
+dd<-data.frame(TempA=jags_dat$TempA, TempS=jags_dat$TempS, Tdiff=jags_dat$TempS-jags_dat$TempA, 
+							 yfrac=jags_dat$year.frac, detect=factor(jags_dat$detect.liz)) 
+#	     filter(yfrac>9/12)
 
-#ddunfilt<-data.frame(TempA=jags_dat$TempA, TempS=jags_dat$TempS, Tdiff=TempS-TempA, 
-#							 yfrac=jags_dat$year.frac, detect=factor(jags_dat$detect.liz)) 
-#ggplot(ddunfilt, aes(x=yfrac*365, y=Tdiff, col=factor(detect)))+
-#	geom_point()
+ddunfilt<-data.frame(TempA=jags_dat$TempA, TempS=jags_dat$TempS, Tdiff=jags_dat$TempS-jags_dat$TempA, 
+							 yfrac=jags_dat$year.frac, detect=factor(jags_dat$detect.liz), site=jags_dat$site) 
 
-RASTPLOT<-ggplot(out, aes(y=Temp, x=TempA)) +
-	geom_raster(aes(fill=p)) +
-	scale_fill_distiller(type="seq", palette='Reds',  direction=1)+
-	stat_contour(aes(y=Temp, x=TempA, z=p), breaks=c(0.5), col="grey")+
-	labs(y=expression("Tile temperature " ( degree~C)),
+everdet_sites<- unique(ddunfilt$site[which(ddunfilt$detect==1)])
+
+dd <- ddunfilt %>%
+	     filter(site %in% everdet_sites) %>%
+	     filter(yfrac > 9/12 & yfrac<12/12 ) 
+
+RASTPLOT<-ggplot(out, aes(y=Tdiff, x=TempA)) +
+	geom_raster(aes(fill=p), interpolate=FALSE) +
+	scale_fill_distiller(type="seq", palette='OrRd',  direction=1)+
+	geom_contour(aes(z=p), breaks=c(0.5), col=gray(0.3), show.legend = TRUE)+
+	labs(y=expression(Delta*T( degree~C)),
 			 x=expression("Air temperature " ( degree~C))) +
-#	labs('Air Temperature') +
-	scale_x_continuous(limits=c(5, 40), expand = c(0, 0))+
-	scale_y_continuous(limits=c(5, 50), expand = c(0, 0))+
-#	geom_point(data=dd, aes(y=TempS, x=TempA, col=detect), alpha=0.8, size=0.4) +
-	scale_color_manual(values=c("lightblue", "black"))+
-	geom_abline(intercept=0, slope=1) +
+	scale_x_continuous(limits=c(9, 41), expand = c(0, -0.5))+
+	scale_y_continuous(limits=c(-6, 11), expand = c(0, -0.5))+
+	scale_color_manual(values=c("white", "black"))+
+	geom_abline(intercept=0, slope=0, linetype="dashed") +
 	annotate("text", x=40, y=50, label="B", vjust=1.2, hjust=1.1, size=10)+
 	theme_bw()+
-	theme(legend.position=c(0.89, 0.25))+
-	theme(legend.title=element_blank())+
+	theme(legend.position=c(0.11, 0.15))+
+	theme(legend.title.align=0.5)  +
+	theme(legend.key.size = unit(0.25, "cm")) +
+	annotate("text", x=Inf, y=Inf, label="B", vjust=1.2, hjust=1.1, size=10)+
 theme(legend.background=element_rect(colour="black", fill="white", size=0))
-
-#another version with temperature difference on y-axis
-# out2<-out %>%
-# 	filter(Tdiff>-20, Tdiff<30, TempA>5, TempA<40)
-# 
-# ggplot(out2, aes(x=TempA, y=Tdiff)) +
-# 	geom_raster(aes(fill=pdet)) +
-# 	#	stat_contour(breaks=c(0.2, 0.6, 0.76), col="grey")+
-# 	scale_fill_gradientn(colours=rev(terrain.colors(7)))+
-# 	xlab('Air temperature') +
-# 	ylab('Temperature difference') +
-# 	geom_point(data=dd, aes(x=TempA, y=Tdiff, col=detect), alpha=0.8, size=0.8) +
-# 	scale_color_manual(values=c("darkgrey", "black"))+
-# 	ylim(-20, 30)+
-# 	geom_abline(intercept=0, slope=0) +
-# 	theme_bw()
 
 pdf("Figures/detection_plot.pdf", width=4, height=7)
 grid.arrange(SP, RASTPLOT, ncol=1, nrow=2)
